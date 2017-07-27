26661de394444f2214f5cfc33b9565fc3967ef84
Victor Makarenkov <vitiokm@gmail.com>
2017-07-27 18:31:14 +0300

    Add files via upload

--- MIMIC3_Notes_Extractor.py
+++ MIMIC3_Notes_Extractor.py
@@ -1,7 +1,7 @@
 import csv
 import StringIO
 
-notes_filename = "/Users/macbook/Desktop/corpora/MIMIC/NOTEEVENTS_example.csv"
+notes_filename_example = "/Users/macbook/Desktop/corpora/MIMIC/NOTEEVENTS_example.csv"
 
 new_only_notes_filename = "/Users/macbook/Desktop/corpora/MIMIC/notes.txt"
 
@@ -33,6 +33,7 @@ def attempt_find_section(text, start_string, stop_list):
             section = low_text[start_index:]
     return section
 
+
 with open(new_only_notes_filename, "a") as new_file:
     with open(complete_mimic_filename) as notes_file:
         data = notes_file.read()
@@ -41,26 +42,12 @@ with open(new_only_notes_filename, "a") as new_file:
         for line in reader:
             line = [x.strip() for x in line]
             if len(line):                                                                                
-                new_file.write("<START_NOTE> ")
-                new_file.write("ID: " + line[1] + "_" + line[2] + "_" + line[3] + " ")
-                new_file.write("Section1: " + attempt_find_section(line[10], start_string_1, end_strings_list1) + " ")
-                new_file.write("Section2: " + attempt_find_section(line[10], start_string_2, end_strings_list2) + " ")
-                new_file.write("<END_NOTE> \n")
+                if (line[6].lower() == "discharge summary"):
+                    new_file.write("<START_NOTE> ")
+                    new_file.write("ID: " + line[1] + "_" + line[2] + "_" + line[3] + " ")
+                    new_file.write("Section1: " + attempt_find_section(line[10], start_string_1, end_strings_list1) + " ")
+                    new_file.write("Section2: " + attempt_find_section(line[10], start_string_2, end_strings_list2) + " ")
+                    new_file.write("<END_NOTE> \n")
 
 
-'''
-# Use this to check and count the lines in a file
-count = 0
-with open(new_only_notes_filename, "a") as new_file:
 
-    with file(notes_filename) as notes_file:
-
-        for line in notes_file:
-            line_parts = line.split(",")
-            note_parts = line_parts[10:]
-            complete_note = ",".join(note_parts)
-            new_file.write(complete_note + "\n")
-           # print "Count", len(line_parts), count, len(note_parts), len(complete_note)
-            count += 1
-print "COUNT:", count
-'''
